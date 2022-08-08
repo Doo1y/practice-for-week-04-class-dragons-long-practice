ca8c90fbbdeafe87373ee889fff3af06a1e25fa3
Samuel Park <s.park111497@gmail.com>
2022-08-08 15:09:08 -0700

    initital commit

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,19 @@
-// Your code here
+
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...instances) {
+    return instances.reduce((acc, curr) => acc.concat(curr.name), []);
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
